604637b323a1815f8dbedef362cee5a36cafcc00
Assassin4b <45405580+Wawwior@users.noreply.github.com>
2021-11-28 20:06:39 +0100

    Initial commit

---
+++ Dijkstra/Graph/Node.cs
@@ -0,0 +1,24 @@
+﻿namespace Dijkstra.Graph
+{
+    public class Node
+    {
+        public Node(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+        public int Distance { get; set; } = -1;
+        public string Previous { get; set; } = "";
+        public bool Iterated { get; set; } = false;
+
+        public override string ToString()
+        {
+            return "Node{" +
+                   "name=" + Name +
+                   ", distance=" + Distance +
+                   ", previous='" + Previous + '\'' +
+                   '}';
+        }
+    }
+}
---
+++ Dijkstra/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Dijkstra
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            long time = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            Console.WriteLine(string.Join(Environment.NewLine, new Dijkstra.Dijkstra(Example()).FindPaths("A")));
+            Console.WriteLine(new Dijkstra.Dijkstra(Example()).FindPath("A", "G"));
+            Console.WriteLine(DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond - time);
+            Console.ReadKey();
+        }
+
+        private static Graph.Graph Example()
+        {
+            Graph.Graph graph = new Graph.Graph();
+            graph.AddNode("A");
+            graph.AddNode("B");
+            graph.AddNode("C");
+            graph.AddNode("D");
+            graph.AddNode("E");
+            graph.AddNode("F");
+            graph.AddNode("G");
+            graph.AddNode("H");
+            graph.AddConnection("AB", 1);
+            graph.AddConnection("AC", 1);
+            graph.AddConnection("AH", 2);
+            graph.AddConnection("BE", 1);
+            graph.AddConnection("CD", 7);
+            graph.AddConnection("CF", 6);
+            graph.AddConnection("DG", 3);
+            graph.AddConnection("EG", 4);
+            graph.AddConnection("FG", 2);
+            graph.AddConnection("FH", 5);
+            return graph;
+        }
+    }
+}
